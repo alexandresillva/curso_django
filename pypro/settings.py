5b9a714c3f0645de8e5b26d3b0150bba060a46eb
alexandresillva <alexandresillva@gmail.com>
2020-06-29 22:12:06 -0300

    Ajuste das configurações de static foldes

--- pypro/settings.py
+++ pypro/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
@@ -123,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-# COLLECTFAST_ENABLED = False
+COLLECTFAST_ENABLED = False
 
 # storage configuration in S3 AWS
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
@@ -137,7 +138,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
-    # COLLECTFAST_ENABLED = True
+    COLLECTFAST_ENABLED = True
     AWS_DEFAULT_ACL = 'private'
 
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
