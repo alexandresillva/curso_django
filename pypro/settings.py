2bf9087ff50ed7884616c8295be617f58be61c52
alexandresillva <alexandresillva@gmail.com>
2020-06-29 23:33:06 -0300

    Ajuste COLLECTFAST_STRATEGY v3

--- pypro/settings.py
+++ pypro/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'collectfast',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
@@ -113,7 +113,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-# COLLECTFAST_ENABLED = False
+COLLECTFAST_ENABLED = False
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
@@ -128,7 +128,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_S3_CUSTOM_DOMAIN = None
 
     # COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
-    # COLLECTFAST_ENABLED = True
+    COLLECTFAST_ENABLED = True
     AWS_DEFAULT_ACL = 'private'
 
     # Static Assets
