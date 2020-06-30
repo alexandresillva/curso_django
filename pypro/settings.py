cfbdabde8b6962c0d77b9f597d858fe46d11414e
alexandresillva <alexandresillva@gmail.com>
2020-06-29 21:13:47 -0300

    Ajuste da publicação no S3 AWS para carregar somente os arquivos ainda não existente no S3

--- pypro/settings.py
+++ pypro/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast'
     'django.contrib.staticfiles',
     'pypro.base',
 ]
@@ -125,6 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLED = False
+
 # storage configuration in S3 AWS
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
@@ -136,6 +139,8 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_DEFAULT_ACL = 'private'
+
+    COLLECTFAST_ENABLED = True
 
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
     STATIC_S3_PATH = 'static'
