b94ccb53fcb0daeeed32bf6157b983a811d728c1
alexandresillva <alexandresillva@gmail.com>
2020-06-29 22:35:13 -0300

    Ajuste das configurações de static folders final

--- pypro/settings.py
+++ pypro/settings.py
@@ -76,7 +76,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pypro.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
